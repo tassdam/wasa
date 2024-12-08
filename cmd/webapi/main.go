32a3a915a8a74568ad59f85887a31e4daeed71b5
Damir Tassybayev <tassybayev.kostanay@gmail.com>
2024-12-08 19:27:50 +0100

    Corrected the imports in main.go

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -28,17 +28,18 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/globaltime"
-	"github.com/ardanlabs/conf"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/sirupsen/logrus"
 	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/ardanlabs/conf"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/sirupsen/logrus"
+	"github.com/tassdam/wasa/service/api"
+	"github.com/tassdam/wasa/service/database"
+	"github.com/tassdam/wasa/service/globaltime"
 )
 
 // main is the program entry point. The only purpose of this function is to call run() and set the exit code if there is
@@ -68,7 +69,6 @@ func run() error {
 		}
 		return err
 	}
-
 	// Init logging
 	logger := logrus.New()
 	logger.SetOutput(os.Stdout)
