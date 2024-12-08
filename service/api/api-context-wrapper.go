aa7c10baf34f6ef9ce63c4c6aff604e8a3357980
Damir Tassybayev <tassybayev.kostanay@gmail.com>
2024-12-08 19:28:55 +0100

    Corrected names of imports in api-context-wrapper.go

--- service/api/api-context-wrapper.go
+++ service/api/api-context-wrapper.go
@@ -1,10 +1,10 @@
 package api
 
 import (
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
 	"github.com/gofrs/uuid"
 	"github.com/julienschmidt/httprouter"
 	"github.com/sirupsen/logrus"
+	"github.com/tassdam/wasa/service/api/reqcontext"
 	"net/http"
 )
 
