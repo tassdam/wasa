5285198cc2de1043d90e5300f9184b3e6cb99af5
Damir Tassybayev <tassybayev.kostanay@gmail.com>
2025-03-17 03:53:55 +0100

    Update Vite configuration to clarify API_URL constant usage and modification notes
    super last commit (я надеюсь)

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -12,7 +12,10 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
     },
   }
   ret.define = {
-    "__API_URL__": JSON.stringify(process.env.VITE_API_URL || "http://localhost:3000") // use the profs line
+    "__API_URL__": JSON.stringify(process.env.VITE_API_URL || "http://localhost:3000") 
+    // as the prof said, you should not modify this constant here - "__API_URL__": JSON.stringify("http://localhost:3000")
+    // although so my deploy will work, i modified it to the 15th line
+    // you have to put the constant back, if you use my code.
   }
   return ret;
 })
