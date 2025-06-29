90614c16637656161c3f9a0d70c6ba78865796cd
Damir Tassybayev <tassybayev.kostanay@gmail.com>
2025-06-29 15:59:21 +0200

    fix: no deployment

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -12,10 +12,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
     },
   }
   ret.define = {
-    "__API_URL__": JSON.stringify(process.env.VITE_API_URL || "http://localhost:3000") 
-    // as the prof said, you should not modify this constant here - "__API_URL__": JSON.stringify("http://localhost:3000")
-    // although so my deploy will work, i modified it to the 15th line
-    // you have to put the constant back, if you use my code.
+    "__API_URL__": JSON.stringify("http://localhost:3000") 
   }
   return ret;
 })
